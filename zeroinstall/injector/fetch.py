2181f8494074fa8632792a1b2dc6945cf23bc8e6
Tim Cuthbertson <tim@gfxmonk.net>
2013-08-26 10:17:21 +1000

    Allow local file href in <file> step
    Cherry-pick of cf4878f248fc2526d60e3695fd4aa81573a8c53c

--- zeroinstall/injector/fetch.py
+++ zeroinstall/injector/fetch.py
@@ -488,11 +488,33 @@ class Fetcher(object):
 
 		# delegate extracting archives to external tool
 		import subprocess
-		subprocess.call([self.external_store, "add", required_digest] + args)
+		retval = subprocess.call([self.external_store, "add", required_digest] + args)
 
 		# delete temp files
 		for path in paths:
 			os.remove(path)
+
+		if retval != 0:
+			raise SafeException(_("Extracting with external store failed"))
+
+	def _download_local_file(self, download_source, impl_hint):
+		# Relative path
+		if impl_hint is None or not impl_hint.feed.local_path:
+			raise SafeException(_("Relative URL '{url}' in non-local feed '{feed}'").format(
+				url = download_source.url,
+				feed = impl_hint.feed))
+
+		local_file = os.path.join(os.path.dirname(impl_hint.feed.local_path), download_source.url)
+		try:
+			size = os.path.getsize(local_file)
+			if size != download_source.size:
+				raise SafeException(_("Wrong size for {path}: feed says {expected}, but actually {actual} bytes").format(
+					path = local_file,
+					expected = download_source.size,
+					actual = size))
+			return (None, open(local_file, 'rb'))
+		except OSError as ex:
+			raise SafeException(str(ex))	# (error already includes path)
 
 	# (force is deprecated and ignored)
 	def download_archive(self, download_source, force = False, impl_hint = None, may_use_mirror = False):
@@ -515,23 +537,7 @@ class Fetcher(object):
 			unpack.check_type_ok(mime_type)
 
 		if '://' not in download_source.url:
-			# Relative path
-			if impl_hint is None or not impl_hint.feed.local_path:
-				raise SafeException(_("Relative URL '{url}' in non-local feed '{feed}'").format(
-					url = download_source.url,
-					feed = impl_hint.feed))
-
-			archive_file = os.path.join(os.path.dirname(impl_hint.feed.local_path), download_source.url)
-			try:
-				size = os.path.getsize(archive_file)
-				if size != download_source.size:
-					raise SafeException(_("Wrong size for {path}: feed says {expected}, but actually {actual} bytes").format(
-						path = archive_file,
-						expected = download_source.size,
-						actual = size))
-				return (None, open(archive_file, 'rb'))
-			except OSError as ex:
-				raise SafeException(str(ex))	# (error already includes path)
+			return self._download_local_file(download_source, impl_hint)
 
 		if may_use_mirror:
 			mirror = self._get_archive_mirror(download_source)
@@ -555,6 +561,9 @@ class Fetcher(object):
 		@rtype: tuple"""
 		if self.config.handler.dry_run:
 			print(_("[dry-run] downloading file {url}").format(url = download_source.url))
+
+		if '://' not in download_source.url:
+			return self._download_local_file(download_source, impl_hint)
 
 		dl = self.download_url(download_source.url, hint = impl_hint)
 		dl.expected_size = download_source.size
